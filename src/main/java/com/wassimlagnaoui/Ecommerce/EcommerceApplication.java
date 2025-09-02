e67d9ee77317404541a4635704d0f24f5e76c31a
wl0182 <lagnaouiw@gmail.com>
2025-09-02 13:40:13 +0100

    Load environment variables from .env file in EcommerceApplication

--- src/main/java/com/wassimlagnaoui/Ecommerce/EcommerceApplication.java
+++ src/main/java/com/wassimlagnaoui/Ecommerce/EcommerceApplication.java
@@ -1,5 +1,6 @@
 package com.wassimlagnaoui.Ecommerce;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+
+		// Set system properties from .env file
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
